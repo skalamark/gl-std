5542d30c8c2289c718e793d98bdd4280467aac1a
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-03-29 22:53:55 -0300

    create struct and impl: Std

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,12 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
+
+use gl_core::preludes::*;
+
+pub struct Std {}
+
+impl Std {
+	pub fn new() -> Env {
+		let env: Env = Env::new();
+		env
+	}
+}
