9164e28d21d51afb1d011f9fe069f905de42ef51
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-04-08 21:42:17 -0300

    create fn: load

--- src/preludes/mod.rs
+++ src/preludes/mod.rs
@@ -1,1 +1,10 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
+
+use gl_core::preludes::*;
+
+mod print_println;
+
+pub fn load(env: &mut Env) {
+	env.set("print", Object::Builtin(format!("print"), -1, print_println::print));
+	env.set("println", Object::Builtin(format!("println"), -1, print_println::println));
+}
