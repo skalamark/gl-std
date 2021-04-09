c527a5661012a0e46da5478edf03edfd2cff194e
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-04-08 22:01:12 -0300

    create fn: input

--- src/preludes/input.rs
+++ src/preludes/input.rs
@@ -1,1 +1,25 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
+
+use std::io::Write;
+
+use gl_core::preludes::*;
+
+pub fn input(args: Vec<Object>) -> Result<Object, Exception> {
+	if args.len() > 0 {
+		print!("{}", match &args[0] {
+			Object::String(s) => s.clone(),
+			o => format!("{}", o),
+		});
+
+		match std::io::stdout().flush() {
+			Ok(_) => Ok(Object::Null),
+			Err(err) => Err(Exception::in_runtime(Except::error(format!("{}", err)))),
+		}?;
+	}
+
+	let mut line = String::new();
+	match std::io::stdin().read_line(&mut line) {
+		Ok(_) => Ok(Object::String(line)),
+		Err(err) => Err(Exception::in_runtime(Except::error(format!("{}", err)))),
+	}
+}
--- src/preludes/mod.rs
+++ src/preludes/mod.rs
@@ -2,9 +2,11 @@
 
 use gl_core::preludes::*;
 
+mod input;
 mod print_println;
 
 pub fn load(env: &mut Env) {
 	env.set("print", Object::Builtin(format!("print"), -1, print_println::print));
 	env.set("println", Object::Builtin(format!("println"), -1, print_println::println));
+	env.set("input", Object::Builtin(format!("input"), -1, input::input));
 }
