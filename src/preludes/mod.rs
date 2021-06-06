7d824c50a7f92acc8408a5f730d6bb48b123c538
JoseCarlosSkar <josecarlosskar@gmail.com>
2021-06-06 19:27:06 -0300

    create fn: type_object

--- src/preludes/mod.rs
+++ src/preludes/mod.rs
@@ -4,9 +4,11 @@ use gl_core::preludes::*;
 
 mod input;
 mod print_println;
+mod type_object;
 
 pub fn load(env: &mut Env) {
 	env.set("print", Object::Builtin(format!("print"), -1, print_println::print));
 	env.set("println", Object::Builtin(format!("println"), -1, print_println::println));
 	env.set("input", Object::Builtin(format!("input"), -1, input::input));
+	env.set("type", Object::Builtin(format!("type"), 1, type_object::type_object));
 }
--- src/preludes/type_object.rs
+++ src/preludes/type_object.rs
@@ -1,1 +1,11 @@
 // Copyright 2021 the GLanguage authors. All rights reserved. MIT license.
+
+use gl_core::preludes::*;
+
+pub fn type_object(args: Vec<Object>) -> Result<Object, Exception> {
+	if args.len() > 0 {
+		Ok(Object::String(args[0].typer().to_string()))
+	} else {
+		Ok(Object::Null)
+	}
+}
